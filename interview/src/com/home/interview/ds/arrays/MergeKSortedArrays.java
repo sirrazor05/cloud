ddff699f25c89ffdaa738279449197fe2ee6759e
sirrazor05 <valentin.butoianu@gmail.com>
2021-01-03 17:51:23 +0100

    remove deprecated

---
+++ interview/src/com/home/interview/ds/arrays/MergeKSortedArrays.java
@@ -0,0 +1,8 @@
+package com.home.interview.ds.arrays;
+
+//https://www.geeksforgeeks.org/merge-k-sorted-arrays/
+//Given k sorted arrays of size n each, merge them and print the sorted output.
+
+public class MergeKSortedArrays {
+
+}
