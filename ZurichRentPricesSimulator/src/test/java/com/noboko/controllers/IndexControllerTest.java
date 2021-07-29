b33f008175716e90f0d046ffc7e07f5288b46f21
sirrazor05 <valentin.butoianu@gmail.com>
2021-07-30 00:30:26 +0200

    add some dummy tests

--- ZurichRentPricesSimulator/src/test/java/com/noboko/controllers/IndexControllerTest.java
+++ ZurichRentPricesSimulator/src/test/java/com/noboko/controllers/IndexControllerTest.java
@@ -1,12 +1,40 @@
 package com.noboko.controllers;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class IndexControllerTest {
 
 	@Test
 	public void test() {
+		assertTrue(false);
 
+	}
+
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test3() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test4() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test5() {
+		assertTrue(true);
 	}
 
 }
